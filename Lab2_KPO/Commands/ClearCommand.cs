159a799b40bc53b47218ab01d811731198a0e269
Oksana  Budko <budkoksana@gmail.com>
2025-05-12 18:29:05 +0300

    Refactoring logic of using pattern command

---
+++ Lab2_KPO/Commands/ClearCommand.cs
@@ -0,0 +1,22 @@
+﻿using Lab2_KPO.Services;
+
+namespace Lab2_KPO.Commands;
+
+public class ClearCommand(MathExpressionValidator validator) : CalculatorCommand(validator)
+{
+    private string _previousExpression;
+
+    public override void Execute()
+    {
+        _previousExpression = Validator.Expression;
+        Validator.Clear();
+    }
+
+    public override void Undo()
+    {
+        foreach (var c in _previousExpression)
+        {
+            Validator.TryAddChar(c);
+        }
+    }
+}
---
+++ Lab2_KPO/Commands/CommandInvoker.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab2_KPO.Commands;
+
+public class CommandInvoker
+{
+    private readonly Stack<ICalculatorCommand> _undoStack = new();
+
+    public void ExecuteCommand(ICalculatorCommand command)
+    {
+        if (command.CanExecute())
+        {
+            command.Execute();
+            _undoStack.Push(command);
+        }
+    }
+
+    public void Undo()
+    {
+        if (_undoStack.Count > 0)
+        {
+            var command = _undoStack.Pop();
+            command.Undo();
+        }
+    }
+
+    public void ClearHistory() => _undoStack.Clear();
+}
